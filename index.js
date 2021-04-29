f5b9d2d3410bde26ceb701d91ed5f32c202ddc5f
Hrithik Mani <hrihtik.mani2000@gmail.com>
2021-04-30 03:33:30 +0530

    Feat: addded frontend sales page, product page

--- index.js
+++ index.js
@@ -17,6 +17,53 @@ res.render("index.ejs");
 
 
 
+//views
+app.get("/home", (req,res) => {
+    //home page
+})
+
+
+
+
+//requests
+app.post("/addProduct", (req,res) => {
+    //Add Product
+    res.send("hi");
+})
+
+app.post("/removeProduct", (req,res) => {
+    //Remove Product
+})
+
+app.post("/updateProduct", (req,res) => {
+    //Update Product
+})
+
+
+app.get("/getAllProducts", (req,res) => {
+    //get All products
+    res.render("product.ejs");
+})
+
+app.get("/getProductById/:id", (req,res) => {
+    //get product By id
+    res.send(req.params.id);
+})
+
+app.get("/sales",(req,res)=>{
+    res.render("sales.ejs");
+})
+
+app.get("/report",(req,res)=>{
+    res.render("salesgraph.ejs");
+})
+
+
+
+
+
+
+
 app.listen(3000,()=>{
     console.log("Running on 3000");
 })
