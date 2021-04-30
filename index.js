3fcba3e3e8da4bab05cb050ad07e18d4de434407
Hrithik Mani <hrihtik.mani2000@gmail.com>
2021-04-30 21:38:09 +0530

    Feat: Added update,sold functionality

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser=require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 const MongoClient =require('mongodb').MongoClient;
+const { ObjectID } = require("mongodb");
 app.use(express.static('public'));
 app.use('/node', express.static(__dirname + '/node_modules'));
 
@@ -33,6 +34,30 @@ app.get("/addProduct", (req,res) => {
     //home page
     res.render("addproduct.ejs");
 })
+
+app.get("/sales",(req,res)=>{
+    res.render("sales.ejs");
+})
+
+app.get("/report",(req,res)=>{
+    res.render("report.ejs");
+})
+
+app.get("/salesgraph", (req,res)=>{
+    res.render("salesgraph.ejs");
+})
+
+app.get("/getAllProducts", (req,res) => {
+    //get All products
+     db.collection("products").find().toArray((err,data)=> {
+        res.render("product.ejs",{res:data});
+     });
+     
+   
+    
+   
+})
+
 
 
 
@@ -67,44 +92,117 @@ app.post("/addProduct", (req,res) => {
    
 })
 
-app.post("/removeProduct", (req,res) => {
+app.post("/removeProduct/:id", (req,res) => {
     //Remove Product
-})
+    var id = ObjectID(req.params.id);
+    db.collection("products").deleteOne({"_id":id},function(err,data){
+        if(data){
+            res.send("1");
+        }else{
+            res.send("0");
+        }
+    });
 
-app.post("/updateProduct", (req,res) => {
+});
+
+app.post("/updateProduct/:id", (req,res) => {
     //Update Product
-})
-
-
-app.get("/getAllProducts", (req,res) => {
-    //get All products
-     db.collection("products").find().toArray((err,data)=> {
-        res.render("product.ejs",{res:data});
-     });
-     
-   
+    var id = ObjectID(req.params.id);
     
-   
+    db.collection("products").updateOne({"_id":ObjectID(req.params.id)},
+    {
+        $set:req.body
+    },
+    function(err,data){
+        if(err) throw err;
+        console.log("Updated");
+        if(data){
+        res.send("1");
+        }else{
+            res.send("0");
+        }
+    }
+    );
+
+
 })
+
+
 
 app.get("/getProductById/:id", (req,res) => {
     //get product By id
-    res.send(req.params.id);
+    db.collection("products").findOne({"_id":ObjectID(req.params.id)},(err,data)=>{
+        if(err){
+            console.log(err);
+            return
+        }
+        if(data != null){
+            res.send(data);
+        }
+
+    });
+
+    
 })
 
-app.get("/sales",(req,res)=>{
-    res.render("sales.ejs");
+
+app.post("/updateSales/:id",function(req,res){
+    var da = new Date();
+    
+    var s = da.getDay();
+
+    var x = ObjectID(req.params.id);
+    db.collection("products").updateOne({"_id":x},{
+        $set:{"sales":{"date":req.body.date,"quantity":req.body.qty}}
+    },function(err,data){
+        if(err) throw err;
+        console.log("Updated");
+        if(data){
+
+           res.send("1");
+
+        
+        }else{
+            res.send("0");
+        }
+    })
 })
 
-app.get("/report",(req,res)=>{
-    res.render("report.ejs");
-})
 
-app.get("/salesgraph", (req,res)=>{
-    res.render("salesgraph.ejs");
-})
+app.post("/decSales/:id",function(req,res){
+  
+    db.collection("products").findOne({"_id":ObjectID(req.params.id)},(err,data)=>{
+        if(err){
+            console.log(err);
+            return
+        }
+        if(data != null){
+            
+            db.collection("products").updateOne({"_id":ObjectID(req.params.id)},
+            {
+                $set:{"quantity":Number(data.quantity)-Number(req.body.value)}
+            },
+            function(err,data2){
+                if(err) throw err;
+                console.log("Updated");
+                if(data2){
+                res.send("1");
+                }else{
+                    res.send("0");
+                }
+            }
+            );
+
+        }
+
+    });
 
 
+
+  
+
+
+});
 
 
 
