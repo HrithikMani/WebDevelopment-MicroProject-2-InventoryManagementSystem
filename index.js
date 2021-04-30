36a472a3b62fb7e97aa6034a509372c81f06fc8c
Hrithik Mani <hrihtik.mani2000@gmail.com>
2021-04-30 07:07:59 +0530

    Feat : add product, Display products completed

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ const app=express();
 const bodyParser=require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-
+const MongoClient =require('mongodb').MongoClient;
 app.use(express.static('public'));
 app.use('/node', express.static(__dirname + '/node_modules'));
 
 
 app.use(express.static('node_modules'));
+
+var db;
+
+MongoClient.connect('mongodb://localhost:27017/inventory',(err,database)=>{
+    if(err)
+    return console.log(err);
+    console.log("Connected to database");
+    db=database.db("inventory");
+})
+
+
 
 app.get("/",(req,res) => {
 res.render("index.ejs");
@@ -28,7 +39,31 @@ app.get("/addProduct", (req,res) => {
 
 //requests
 app.post("/addProduct", (req,res) => {
+
     //Add Product
+    
+    db.collection("products").findOne({id:req.body.id},(err,data)=>{
+    if(err){
+        console.log(err);
+        return;
+    }   
+    
+    if(data == null){
+        db.collection("products").insertOne(req.body,function(err){
+            if(err){
+                console.log(err);
+                res.send("0");
+            }else{
+                console.log("Added");
+                res.send("1");
+            }
+
+        });
+      
+    }else{
+        res.send("0");
+    }
+   });
    
 })
 
@@ -43,7 +78,13 @@ app.post("/updateProduct", (req,res) => {
 
 app.get("/getAllProducts", (req,res) => {
     //get All products
-    res.render("product.ejs");
+     db.collection("products").find().toArray((err,data)=> {
+        res.render("product.ejs",{res:data});
+     });
+     
+   
+    
+   
 })
 
 app.get("/getProductById/:id", (req,res) => {
